64f4e815be2bd5f28ff760780dee745ffdf42d29
Wenzel Whilan <wenzelcruzado14@gmail.com>
2025-02-21 14:27:53 -0500

    Iniciando el API call twilio test

---
+++ test/server/src/services/call_twilio.py
@@ -0,0 +1,16 @@
+
+from src.config.config_twilio import client, TWILIO_PHONE_NUMBER
+
+def make_call(phone_number):
+
+    # Hace una llamada a un número específico usando Twilio.
+
+    try:
+        call = client.calls.create(
+            twiml='<Response><Say>Hola, esta es una llamada de prueba desde Twilio.</Say></Response>',
+            to=phone_number,
+            from_=TWILIO_PHONE_NUMBER
+        )
+        return {"success": True, "call_sid": call.sid}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
